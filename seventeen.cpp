3e81a60fa342cd898f678b362653d864008ef8b9
domaksym69@gmail.com <domaksym69@gmail.com>
2024-11-21 21:26:13 +0200

    Add project files.

---
+++ seventeen.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class stringg {
+    string st;
+public:
+    static int count;
+    stringg(): st("qwertyui") { count++; }
+    stringg(string d): st(d){ count++; }
+    stringg(const stringg& other) {
+        this->st = other.st;
+        count++;
+    }
+    void show() {
+        cout << st << endl;
+    }
+};
+
+int stringg::count = 0;
+
+int main()
+{
+    stringg st1;
+    stringg st2("fdfds");
+    stringg st3(st2);
+    st1.show();
+    st2.show();
+    st3.show();
+    cout << stringg::count;
+}
